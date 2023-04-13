a86d6d02748f0c0baea47294fbbcd38dd0669fe7
Frid Matilda <matilda.x.frid@scania.com>
2023-04-13 23:13:56 +0200

    basic styling of summary page

--- src/components/Summary/Counter.js
+++ src/components/Summary/Counter.js
@@ -1,8 +1,29 @@
 /* eslint-disable max-len */
 import React from 'react';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
-import './Summary.css';
+const StyledCounter = styled.div`
+    background: transparent;
+    width: 100%;
+    height: 15%;
+`;
+
+const StyledResult = styled.div`
+    position: relative;
+    font-size: 32px;
+    z-index: 100;
+    text-align: center;
+    margin: 2rem;
+`;
+
+const StyledWave = styled.svg`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: auto;
+    transform: rotate(180deg);
+`;
 
 export const Counter = () => {
   const storeAnswer = useSelector((state) => state.quiz.answers)
@@ -10,19 +31,16 @@ export const Counter = () => {
     (answer) => answer.isCorrect === true
   )
   return (
-    <div className="counter">
-      <div className="wave-bg">
-        WAVE HERE?
-      </div>
-      <div className="result">
+    <StyledCounter>
+      <StyledWave xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#B8F1E6" fillOpacity="1" d="M0,160L34.3,144C68.6,128,137,96,206,96C274.3,96,343,128,411,160C480,192,549,224,617,208C685.7,192,754,128,823,117.3C891.4,107,960,149,1029,181.3C1097.1,213,1166,235,1234,234.7C1302.9,235,1371,213,1406,202.7L1440,192L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z" />
+      </StyledWave>
+      <StyledResult>
         <h1>You got {correctAnswers.length} out of {storeAnswer.length}</h1>
         {correctAnswers.length < 4 && (<p>Better luck next time!</p>)}
         {correctAnswers.length >= 4 && correctAnswers.length < 6 && (<p>Not too bad! Practice a little more for next time.</p>)}
         {correctAnswers.length >= 7 && (<p>You are a star! Excellent trivia skills!</p>)}
-      </div>
-    </div>
+      </StyledResult>
+    </StyledCounter>
   )
 }
-
-// Stretch goal - ternary operator to give different messages depending
-// on the amount of correct answers?
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,23 +1,73 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX, faMedal } from '@fortawesome/free-solid-svg-icons';
 import { Counter } from './Counter';
 import { RestartBtn } from '../RestartBtn';
 
-import './Summary.css';
+const StyledBackground = styled.div`
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(0deg, hsla(168, 67%, 83%, 1) 0%, hsla(0, 0%, 100%, 1) 100%);  
+`;
+
+const StyledAnswerWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2rem;
+  flex-wrap: wrap;
+  padding: 2rem;
+  margin-top: 5rem;
+`;
+
+const StyledSummaryCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  background: ${(props) => {
+    if (props.questionId === 1) {
+      return 'linear-gradient(180deg, hsla(46, 75%, 92%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 2) {
+      return 'linear-gradient(180deg, hsla(5, 100%, 81%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 3) {
+      return 'linear-gradient(180deg, hsla(187, 44%, 78%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 4) {
+      return 'linear-gradient(180deg, hsla(168, 67%, 83%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 5) {
+      return 'linear-gradient(180deg, hsla(46, 75%, 92%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 6) {
+      return 'linear-gradient(180deg, hsla(5, 100%, 81%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 7) {
+      return 'linear-gradient(180deg, hsla(187, 44%, 78%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 8) {
+      return 'linear-gradient(180deg, hsla(168, 67%, 83%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 9) {
+      return 'linear-gradient(180deg, hsla(46, 75%, 92%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else if (props.questionId === 10) {
+      return 'linear-gradient(180deg, hsla(5, 100%, 81%, 1) 0%, hsla(0, 0%, 100%, 1) 100%)';
+    } else {
+      return 'inherit';
+    }
+  }};
+  width: 320px;
+  height: 300px;
+  padding: 1rem;
+  box-shadow: 5px 6px 23px 1px rgba(0,0,0,0.3);
+`;
 
 export const Summary = () => {
   const storeAnswer = useSelector((state) => state.quiz.answers)
   return (
-    <div className="summary">
+    <StyledBackground>
       <Counter />
-      <div className="answer-wrapper">
+      <StyledAnswerWrapper>
         {storeAnswer.map((results) => {
           return (
-            <div key={results.questionId} className="summary-card">
-              <h1 className="question-title">{results.questionId}</h1>
+            <StyledSummaryCard key={results.questionId} isCorrect={results.isCorrect} questionId={results.questionId}>
+              <h1 className="question-title">{results.questionId}.</h1>
               <h2 className="question-text">{results.question.questionText}</h2>
               <p className="your-answer">{results.answers}</p>
               {results.isCorrect ? (
@@ -30,11 +80,11 @@ export const Summary = () => {
                   (Next time answer - {results.question.options[results.question.correctAnswerIndex]})
                 </p>
               )}
-            </div>
+            </StyledSummaryCard>
           );
         })}
-      </div>
+      </StyledAnswerWrapper>
       <RestartBtn />
-    </div>
+    </StyledBackground>
   )
 }
