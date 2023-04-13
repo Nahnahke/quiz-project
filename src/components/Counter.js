063c7b604ae523afaebcdc6cc4f1efb7f79bacc3
Frid Matilda <matilda.x.frid@scania.com>
2023-04-13 20:33:00 +0200

    added summary css

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,17 +2,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import './Summary.css';
+
 export const Counter = () => {
   const storeAnswer = useSelector((state) => state.quiz.answers)
   const correctAnswers = storeAnswer.filter(
     (answer) => answer.isCorrect === true
   )
   return (
-    <div>
-      <h1>You got {correctAnswers.length} out of {storeAnswer.length}</h1>
-      {correctAnswers.length < 3 && (<p>Better luck next time!</p>)}
-      {correctAnswers.length >= 3 && correctAnswers.length < 6 && (<p>Not too bad! Practice a little more for next time.</p>)}
-      {correctAnswers.length >= 6 && (<p>You are a star! Excellent trivia skills!</p>)}
+    <div className="counter">
+      <div className="wave-bg">
+        WAVE HERE?
+      </div>
+      <div className="result">
+        <h1>You got {correctAnswers.length} out of {storeAnswer.length}</h1>
+        {correctAnswers.length < 4 && (<p>Better luck next time!</p>)}
+        {correctAnswers.length >= 4 && correctAnswers.length < 6 && (<p>Not too bad! Practice a little more for next time.</p>)}
+        {correctAnswers.length >= 7 && (<p>You are a star! Excellent trivia skills!</p>)}
+      </div>
     </div>
   )
 }
